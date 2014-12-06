9993c9ac6e4e33bdb1723d37cfcd80273e6ca89d
psinalberth <inalberth07@gmail.com>
2014-12-06 11:05:50 -0200

    Commit inicial de Salas e Departamentos

---
+++ app/Controller/DepartamentosController.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @author inalberth
+ */
+class DepartamentosController extends AppController {
+	
+	public $name = "Departamentos";
+	
+	public $uses = array('Departamentos');
+	
+	public function index() {
+		
+	}
+	
+	public function view($id = null) {
+		
+	}
+	
+	public function add() {
+		
+	}
+	
+	public function edit($id = null) {
+		
+	}
+	
+	public function delete($id = null) {
+		
+	}
+}
--- app/Controller/SalasController.php
+++ app/Controller/SalasController.php
@@ -10,17 +10,32 @@
  *
  * @author Márcio Vennan
  */
-class SalasController extends AppController{
+class SalasController extends AppController {
     
     public $name = "Salas";
     
-    public $uses = array();
+    public $uses = array('Salas', 'Departamentos');
+    
+    public function index() {
+    	
+    }
+    
+    public function view($id = null) {
+    	
+    }
 
     public function add(){
         //$this->checkAccess( $this->name, __FUNCTION__ );
        pr($_GET);die;
     }
     
+    public function edit($id = null) {
+    	
+    }
+    
+    public function delete($id = null) {
+    	
+    }
+    
 }
-
 ?>
