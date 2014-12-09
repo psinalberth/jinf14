9299cf27b98cddc4327a8daf978d07262d8bc15c
BiancaMaria <bianca.engcomp@gmail.com>
2014-12-09 19:35:30 -0300

    torna o participante um usuario do sistema

---
+++ app/Model/Participante.php
@@ -0,0 +1,47 @@
+<?php
+
+class Participante extends AppModel {
+	
+	/*----------------------------------------
+	 * Atributtes
+	 ----------------------------------------*/ 
+
+	public $name  = 'Participante';
+
+	public $useTable = 'programacao';
+        
+        public $label = 'Usuario';
+	/*----------------------------------------
+	 * Associations
+	 ----------------------------------------*/ 
+	
+        public $hasOne = array( 
+		'User' => array( 
+			'className' => 'User', 
+			'foreignKey' => 'user_id' 
+		)
+	
+);
+	
+	/*----------------------------------------
+	 * Validation
+	 ----------------------------------------*/
+	
+	public $validate = array();
+	
+	/*----------------------------------------
+	 * Methods
+	 ----------------------------------------*/
+	
+        public function beforeValidate( $options = array() ){
+
+	}
+	
+	public function beforeSave(){
+
+		
+	}
+	
+}
+
+?>
