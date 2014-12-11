c8d6081be6151e3f093ed6bd4375462c0fd404e1
Jouglas Willians <jouli1204@gmail.com>
2014-12-11 10:35:56 -0300

    Correção de erro no model de Atividade
    Remoção do "<<<<<<< HEAD" no model de Atividade.

--- app/Model/Atividade.php
+++ app/Model/Atividade.php
@@ -1,78 +1,59 @@
 <?php
 
 class Atividade extends AppModel {
-	
-	/*----------------------------------------
-	 * Atributtes
-	 ----------------------------------------*/ 
-	
-	public $name 			=	'Atividade';
+    /* ----------------------------------------
+     * Atributtes
+      ---------------------------------------- */
 
-	public $useTable		=    'atividade';
-
-	public $label			=	'Atividade';
-
-	
-	/*----------------------------------------
-	 * Associations
-	 ----------------------------------------*/ 
-	public $belongsTo = array( 
-		'TipoAtividade' => array( 
-			'className' => 'TipoAtividade', 
-			'foreignKey' => 'tipo_atividade_id' 
-		)
-	);	
-<<<<<<< HEAD
-	/*public $hasMany = array( 
-		'Agenda' => array( 
-			'className' => 'Agenda', 
-			'foreignKey' => 'atividade_cod_ativ' 
-		)
-	);	*/
-=======
-	//public $hasMany = array( 
-	//	'Agenda' => array( 
-	//		'className' => 'Agenda', 
-	//		'foreignKey' => 'atividade_cod_ativ' 
-	//	)
-	//);	
->>>>>>> 99335e4481c780da3dcd8d0ed6b1939ecbb073d0
-	
-	/*----------------------------------------
-	 * Validation
-	 ----------------------------------------*/
-	
-	public $validate 		= 	array(
-		'nome_atividade' => array('rule' => 'notEmpty','message' => 'Este Campo não pode ser Vazio!'),
-		'tipo_atividade_id' => array('rule' => 'notEmpty','message' => 'Este Campo não pode ser Vazio!'),
-		'duracao' => array('notEmpty' => array('rule' => 'notEmpty','message' => 'Este Campo não pode ser Vazio!'), 'numeric'=> array('rule' =>'numeric', 'message' => 'Somente números')),
-		'vagas' => array('notEmpty' => array('rule' => 'notEmpty','message' => 'Este Campo não pode ser Vazio!'), 'numeric'=> array('rule' =>'numeric', 'message' => 'Somente números'))
-
-	);
-
-	
-	
+    public $name = 'Atividade';
+    public $useTable = 'atividade';
+    public $label = 'Atividade';
 
 
+    /* ----------------------------------------
+     * Associations
+      ---------------------------------------- */
+    public $belongsTo = array(
+        'TipoAtividade' => array(
+            'className' => 'TipoAtividade',
+            'foreignKey' => 'tipo_atividade_id'
+        )
+    );
+    /* public $hasMany = array( 
+        'Agenda' => array(
+            'className' => 'Agenda',
+            'foreignKey' => 'atividade_cod_ativ'
+        )
+      ); */
 
-	/*----------------------------------------
-	 * Methods
-	 ----------------------------------------*/
-	
+    
+    /* ----------------------------------------
+     * Validation
+      ---------------------------------------- */
+    public $validate = array(
+        'nome_atividade' => array('rule' => 'notEmpty', 'message' => 'Este Campo não pode ser Vazio!'),
+        'tipo_atividade_id' => array('rule' => 'notEmpty', 'message' => 'Este Campo não pode ser Vazio!'),
+        'duracao' => array('notEmpty' => array('rule' => 'notEmpty', 'message' => 'Este Campo não pode ser Vazio!'), 'numeric' => array('rule' => 'numeric', 'message' => 'Somente números')),
+        'vagas' => array('notEmpty' => array('rule' => 'notEmpty', 'message' => 'Este Campo não pode ser Vazio!'), 'numeric' => array('rule' => 'numeric', 'message' => 'Somente números'))
+    );
 
-	/*----------------------------------------
-	 * Callbacks
-	 ----------------------------------------*/
+    /* ----------------------------------------
+     * Methods
+      ---------------------------------------- */
 
-	public function beforeValidate( $options = array() ){
 
-	}
-	
-	public function beforeSave(){
+    /* ----------------------------------------
+     * Callbacks
+      ---------------------------------------- */
 
-		
-	}
-	
-}	
+    public function beforeValidate($options = array()) {
+        
+    }
+
+    public function beforeSave() {
+        
+    }
+
+}
 
 ?>
