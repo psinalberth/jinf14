69869544951631337e34d88ef4a3b09533c63a8b
Jouglas Willians <jouli1204@gmail.com>
2014-12-11 18:56:50 -0300

    Corrigindo conflito no model Atividade
    Correção do conflito gerado no model de Atividade.

--- app/Model/Atividade.php
+++ app/Model/Atividade.php
@@ -1,7 +1,7 @@
 <?php
 
 class Atividade extends AppModel {
-<<<<<<< HEAD
+    
     /* ----------------------------------------
      * Atributtes
       ---------------------------------------- */
@@ -54,75 +54,5 @@ class Atividade extends AppModel {
     public function beforeSave() {
         
     }
-
 }
-=======
-	
-	/*----------------------------------------
-	 * Atributtes
-	 ----------------------------------------*/ 
-	
-	public $name 			=	'Atividade';
-
-	public $useTable		=    'atividade';
-
-	public $label			=	'Atividade';
-
-	
-	/*----------------------------------------
-	 * Associations
-	 ----------------------------------------*/ 
-	public $belongsTo = array( 
-		'TipoAtividade' => array( 
-			'className' => 'TipoAtividade', 
-			'foreignKey' => 'tipo_atividade_id' 
-		)
-	);	
-
-	/*public $hasMany = array( 
-		'Agenda' => array( 
-			'className' => 'Agenda', 
-			'foreignKey' => 'atividade_cod_ativ' 
-		)
-	);	*/
-
-	
-	/*----------------------------------------
-	 * Validation
-	 ----------------------------------------*/
-	
-	public $validate 		= 	array(
-		'nome_atividade' => array('rule' => 'notEmpty','message' => 'Este Campo não pode ser Vazio!'),
-		'tipo_atividade_id' => array('rule' => 'notEmpty','message' => 'Este Campo não pode ser Vazio!'),
-		'duracao' => array('notEmpty' => array('rule' => 'notEmpty','message' => 'Este Campo não pode ser Vazio!'), 'numeric'=> array('rule' =>'numeric', 'message' => 'Somente números')),
-		'vagas' => array('notEmpty' => array('rule' => 'notEmpty','message' => 'Este Campo não pode ser Vazio!'), 'numeric'=> array('rule' =>'numeric', 'message' => 'Somente números'))
-
-	);
-
-	
-	
-
-
-
-	/*----------------------------------------
-	 * Methods
-	 ----------------------------------------*/
-	
-
-	/*----------------------------------------
-	 * Callbacks
-	 ----------------------------------------*/
-
-	public function beforeValidate( $options = array() ){
-
-	}
-	
-	public function beforeSave(){
-
-		
-	}
-	
-}	
->>>>>>> origin/master
-
 ?>
